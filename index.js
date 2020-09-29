1bc45de9b721092ada26aa4131535baa9e2237b5
Jon Barson <jbarson@gmail.com>
2020-09-29 14:18:13 -0400

    lab take up

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (_req, res) => res.json('{name: "Jon", level, "Educator"}'));
-app.get('/about', (req, res) => {
-  res.send('This is the about page');
-  console.log(req);
-});
-app.route('/contact')
-  .get((_req, res) => res.send('get a contact'))
-  .post((_req, res) => res.send('create a contact'))
-  .delete((_req, res) => res.send('delete a contact'));
+const ingredients = ['turkey', 'gravy', 'potatoes', 'carrots']; 
 
-  app.get('/shoes',  (req, res) => {
-    console.log(req.query.order);
- 
-    console.log(req.query.shoe.color);
- 
-    console.log(req.query.shoe.type);
- 
-    res.send('stuff');
-});
+app.get('/famousdish', (req, res) => res.send('Turkey dinner'));
 
-app.get('/users/:username', (req, res) => {
-  console.log('the user is: ', req.params.username);
-  const user = req.params.username;
-  let returnedData = user=== 'Lauren' ? 'Educator': 'someone else' 
-  res.send(returnedData);
+app.get('/ingredients', (req, res) => res.json(ingredients));
+
+app.post('/orders', (req, res) => {
+  res.status(418);
+  res.send('We are not taking orders at this time. Our service is temporarily down')
 })
-
-
 
 app.listen(8080,() => console.log('running on port 8080'));
